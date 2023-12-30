b2967e0d750f872be76fd44085e488749b29e501
wankunda sikaonga <sikaongawankunda@gmail.com>
2023-12-30 22:38:25 +0200

    added game logic to print winner after acquiring 3 points

--- functions.js
+++ functions.js
@@ -10,21 +10,62 @@ function getComputerChoice() {
     }
   }
   
+
   function play(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-      return "It's a tie!";
+        let outcome =  "It's a tie!";
+      return outcome;
     } else if (
       (playerSelection === "rock" && computerSelection === "scissors") ||
       (playerSelection === "scissors" && computerSelection === "paper") ||
       (playerSelection === "paper" && computerSelection === "rock")
     ) {
-      return "You won! " + playerSelection + " beats " + computerSelection;
+     let outcome = "You won! ";
+      return outcome
     } else {
-      return "Computer won! " + computerSelection + " beats " + playerSelection;
+      let outcome = "Computer won! ";
+      return outcome 
     }
   }
-  
-  let computerSelection = getComputerChoice();
-  let result = play("rock", computerSelection);
-  console.log(result);
-  
+
+
+  function game(){  
+    
+     let  player_points = 0;
+     let  computer_points = 0;
+
+    //while loop checks for winner.
+    while(player_points < 3 && computer_points < 3){
+
+       //Function calls for getComputerChoice() and play()
+    let computerSelection = getComputerChoice();
+    let result = play("rock", computerSelection);
+
+    //if statements to check for current winner and overall winner
+      //check if player has won 
+      if(result ===( "You won! ")){
+        player_points++
+        console.log("You have: " + player_points + " points")
+        //check is player is overall winner 
+        if(player_points >= 3){
+            console.log("You WIN!")
+        }
+      }
+      //check if computer is current winner 
+      else if (result === ("Computer won! ")){
+       computer_points++
+        console.log("Computer has: " + computer_points + " points")
+        //check if computer is overall winner 
+        if(computer_points >= 3){
+            console.log("COMPUTER WINS!")
+        }
+      }
+      //else statement for tie
+      else{
+        console.log("Zero points for y'all")
+      }
+    }
+  }
+
+ 
+  game()
